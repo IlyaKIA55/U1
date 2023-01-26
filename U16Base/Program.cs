a11fcfa785e444d74525cfafad491633ca4d3abe
Ilya <jyg3a125@gmail.com>
2023-01-26 11:14:31 +0500

    add U17Magic while

--- U16Base/Program.cs
+++ U16Base/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 2;
+int b = 5;
+int c = 8;
+int d = 3;
+int e = 1;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("Максимальное значение= ");
+Console.WriteLine(max);
